fde70c0f93fe4fc59147a9e78d691e767fba42b7
Marc R. Uchniat <logan5@feem.net>
2020-07-12 09:22:41 -0500

    add internet connectivity

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
+const char *clientid = SPOTIFY_ID;
+const char *secret = SPOTIFY_SECRET;
 const char *topbar = "Now Playing [Spotify]";
 const char *artist = "Bagel Man";
 const char *title = "Meatball Sandwich Time";
 
 int xa, xb, minXa, minXb;
+
+bool connected = false;
 
 void advanceframe() {
     display.clearDisplay();
@@ -27,7 +31,7 @@ void advanceframe() {
     display.setTextSize(2);
     display.setCursor(xa, 20);
     display.print(artist);
-    display.setCursor(xb, 35);
+    display.setCursor(xb, 40);
     display.print(title);
     display.display();
     xa = xa - 1;
@@ -46,24 +50,49 @@ void updatemessagelength() {
 }
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    delay(2000);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;);
+    }
+    display.clearDisplay();
+    display.setTextWrap(false);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    xa = display.width();
+    xb = display.width();
+    updatemessagelength();
 
-  display.setTextWrap(false);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  xa = display.width();
-  xb = display.width();
-  updatemessagelength();
-
-  Serial.printf("\nConnecting...");
-
+    display.setCursor(0,7);
+    display.print("Connecting...");
+    Serial.printf("\nConnecting...");
+    display.display();
+    
+    WiFi.begin(ssid, password);
+  
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+    
+    IPAddress ip = WiFi.localIP();
+    
+    Serial.printf("Connected!\nharken is %s.", ip.toString().c_str());
+    display.clearDisplay();
+    display.setCursor(0,7);
+    display.print("Connected!");
+    display.setTextSize(1);
+    display.setCursor(0,24);
+    display.print(ip.toString().c_str());
+    display.display();
+    delay(2000);
+    connected = true;
 }
 
 void loop() {
-    advanceframe();
+    if (connected) {
+        advanceframe();
+    }
 }
